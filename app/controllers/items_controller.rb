05d7885eb44f89a835ff4f1c8f82feebe382e702
JoabOwala <owalajoab@gmail.com>
2023-06-25 16:48:02 +0300

    Assignment Completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+  before_action :set_user
 
+  # GET /users/:user_id/items
   def index
-    items = Item.all
-    render json: items, include: :user
+    items = @user.items
+    render json: items
   end
 
+  # GET /users/:user_id/items/:id
+  def show
+    item = @user.items.find_by(id: params[:id])
+    if item
+      render json: item
+    else
+      render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+
+  # POST /users/:user_id/items
+  def create
+    item = @user.items.build(item_params)
+    if item.save
+      render json: item, status: :created
+    else
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+    render json: { error: "User not found" }, status: :not_found unless @user
+  end
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
 end
